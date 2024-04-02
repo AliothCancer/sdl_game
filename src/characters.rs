1ccd19eb2fd859d6e26348f6c47ab100c107a12e
AliothCancer <ortjuly7@gmail.com>
2024-04-02 19:01:52 +0200

    First commit

---
+++ src/characters.rs
@@ -0,0 +1,84 @@
+use crate::sdl_thing::{HEIGHT, WIDTH};
+use sdl2::{pixels::Color, rect::Rect};
+
+#[derive(Debug)]
+pub enum Controller {
+    Up,
+    UpLeft,
+    UpRight,
+
+    Down,
+    DownLeft,
+    DownRight,
+
+    Left,
+    Right,
+
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub struct Character {
+    name: String,
+    hp: u8,
+    pub rect: Rect,
+    color: Color,
+    speed: i32,
+}
+
+impl Character {
+    pub fn movement(&mut self, control: Controller) {
+        match control {
+            Controller::Down => self.rect.set_y(self.rect.y + self.speed),
+            Controller::Up => self.rect.set_y(self.rect.y - self.speed),
+            Controller::Left => self.rect.set_x(self.rect.x - self.speed),
+            Controller::Right => self.rect.set_x(self.rect.x + self.speed),
+            Controller::DownLeft => {
+                self.rect.set_y(self.rect.y + self.speed);
+                self.rect.set_x(self.rect.x - self.speed);
+            },
+            Controller::DownRight => {
+                self.rect.set_y(self.rect.y + self.speed);
+                self.rect.set_x(self.rect.x + self.speed);
+            }
+            Controller::UpLeft => {
+                self.rect.set_y(self.rect.y - self.speed);
+                self.rect.set_x(self.rect.x - self.speed)
+            }
+            Controller::UpRight => {
+                self.rect.set_y(self.rect.y - self.speed);
+                self.rect.set_x(self.rect.x + self.speed);
+            }
+        }
+    }
+    pub fn default_player(name: String) -> Self {
+        let coor = ((WIDTH / 2) as i32, (HEIGHT / 2) as i32);
+        Character {
+            name,
+            hp: 100,
+            rect: Rect::new(coor.0, coor.1, 100, 100), // Default rect dimensions
+            color: Color::BLUE,                        // Default color
+            speed: 10,
+        }
+    }
+    pub fn new_player(name: String, rect: Rect) -> Self {
+        Character {
+            name,
+            hp: 100,
+            rect,
+            color: Color::BLUE,
+            speed: 10,
+        }
+    }
+
+    pub fn get_rect(&self) -> Rect {
+        self.rect
+    }
+    pub fn set_rect(&mut self, rect: Rect) {
+        self.rect = rect;
+    }
+
+    pub fn get_color(&self) -> Color {
+        self.color
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+pub mod characters;
+pub mod sdl_thing;
+pub mod keyboard_controls;
+
+use characters::Character;
+use sdl2::{self, pixels::Color};
+use std::time::Duration;
+
+use sdl_thing::State;
+
+fn main() {
+    let mut my_window = sdl_thing::get_window();
+    my_window.setup();
+
+    let player = Character::default_player("ValmerWolf".to_string());
+    my_window.add_characters(player);
+
+    let mut i = 0;
+
+    while let State::Continue = my_window.state {
+        i = (i + 1) % 255;
+        my_window.canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        my_window.canvas.clear();
+
+        my_window.from_message_to_action();
+        my_window.draw_characters();
+
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
